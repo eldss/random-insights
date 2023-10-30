2bf4e35a39ec5155fafcf69229f9e063d6e99092
Evan Douglass <evanldouglass@gmail.com>
2023-10-29 22:03:43 -0700

    Add images and icons

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -1,14 +1,27 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Image, ImageStyle } from "react-native";
 import * as React from "react";
 import { MeditationSettingsScreen } from "../screens/MeditationSettingsScreen";
+import { spacing } from "../theme";
 
 const Stack = createNativeStackNavigator();
 
 export function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ title: "Random Insights" }}>
+      <Stack.Navigator
+        screenOptions={{
+          title: "Random Insights",
+          headerBackVisible: true,
+          headerLeft: () => (
+            <Image
+              style={$icon}
+              source={require("../../assets/LotusIcon.png")}
+            />
+          ),
+        }}
+      >
         <Stack.Screen
           name="MeditationSettingsScreen"
           component={MeditationSettingsScreen}
@@ -17,3 +30,9 @@ export function AppNavigator() {
     </NavigationContainer>
   );
 }
+
+const $icon: ImageStyle = {
+  width: 50,
+  height: 50,
+  marginRight: spacing.xs,
+};
