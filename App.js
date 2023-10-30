2466cd0c0afc1b4f0fc8b55e14702a933f7cf861
Evan Douglass <evanldouglass@gmail.com>
2023-10-29 20:47:32 -0700

    Update app.json values

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { MeditationSettingsScreen } from "./src/screens/MeditationSettingsScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AppNavigator } from "./src/navigators";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      {/* <SafeAreaView style={styles.container}>
-        <StatusBar style="auto" />
-        <MeditationSettingsScreen />
-      </SafeAreaView> */}
       <AppNavigator />
     </SafeAreaProvider>
   );
--- src/screens/MeditationSettingsScreen.tsx
+++ src/screens/MeditationSettingsScreen.tsx
@@ -1,65 +1,67 @@
-import { View, Text, ViewStyle, ScrollView } from "react-native";
-import React from "react";
-import { Card } from "../components/Card";
-import { fontSize, spacing } from "../theme";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, ScrollView, Text, View, ViewStyle } from "react-native";
+import { Card } from "../components/Card";
+import { spacing } from "../theme";
 
 export function MeditationSettingsScreen() {
   return (
-    <ScrollView>
-      <StatusBar style="auto" />
-      <View style={$container}>
-        <Card title="Test 1">
-          <Text>Test Body</Text>
-        </Card>
-        <Card title="Test 2">
-          <Text>Test body with longer text</Text>
-        </Card>
-        <Card title="Test 3">
-          <Text>
-            Test body with longer text blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah
-          </Text>
-        </Card>
-        <Card title="Test 4">
-          <Text>
-            Test body with longer text blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah
-          </Text>
-        </Card>
-        <Card title="Test 5">
-          <Text>
-            Test body with longer text blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah ah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah h blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah ah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah
-          </Text>
-        </Card>
-      </View>
-    </ScrollView>
+    <SafeAreaView>
+      <ScrollView>
+        <StatusBar style="auto" />
+        <View style={$container}>
+          <Card title="Test 1">
+            <Text>Test Body</Text>
+          </Card>
+          <Card title="Test 2">
+            <Text>Test body with longer text</Text>
+          </Card>
+          <Card title="Test 3">
+            <Text>
+              Test body with longer text blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah
+            </Text>
+          </Card>
+          <Card title="Test 4">
+            <Text>
+              Test body with longer text blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah
+            </Text>
+          </Card>
+          <Card title="Test 5">
+            <Text>
+              Test body with longer text blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah ah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah h blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah ah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah blah blah blah
+              blah blah blah blah blah blah blah blah blah blah
+            </Text>
+          </Card>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
