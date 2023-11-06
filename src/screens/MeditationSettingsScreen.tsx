5984adaef3dc7a0d8e02839889d03241bb71aa9e
Evan Douglass <evanldouglass@gmail.com>
2023-11-05 23:19:49 -0800

    Install countdown component

--- src/screens/MeditationSettingsScreen.tsx
+++ src/screens/MeditationSettingsScreen.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView, ScrollView, Text, View, ViewStyle } from "react-native";
 import { Card } from "../components/Card";
 import { spacing } from "../theme";
 import { MeditationTypeCard } from "../components";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 
 export function MeditationSettingsScreen() {
   return (
@@ -16,14 +17,17 @@ export function MeditationSettingsScreen() {
             <Text>Test body with longer text</Text>
           </Card>
           <Card title="Test 3">
-            <Text>
-              Test body with longer text blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah
-            </Text>
+            <CountdownCircleTimer
+              isPlaying
+              duration={60}
+              colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+              colorsTime={[7, 5, 2, 0]}
+              onComplete={() => {
+                return { shouldRepeat: true, delay: 1.5 };
+              }}
+            >
+              {({ remainingTime }) => <Text>{remainingTime}</Text>}
+            </CountdownCircleTimer>
           </Card>
           <Card title="Test 4">
             <Text>
