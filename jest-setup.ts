3d94ca95bcd5fb24b50940ed42b6dee5929b5fa3
Evan Douglass <evanldouglass@gmail.com>
2023-11-25 00:53:52 -0800

    Move common localization mock to jest setup file

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-native/extend-expect";
+
+// Ensure localization gets English and prevent undefined errors
+jest.mock("expo-localization", () => {
+  const original = jest.requireActual("expo-localization");
+  return {
+    __esModule: true,
+    ...original,
+    getLocales: () => {
+      return [
+        {
+          languageCode: "en",
+        },
+      ];
+    },
+  };
+});
--- src/navigators/__tests__/AppNavigator.test.tsx
+++ src/navigators/__tests__/AppNavigator.test.tsx
@@ -1,18 +1,6 @@
 import { render, screen, userEvent } from "@testing-library/react-native";
 import { AppNavigator } from "../AppNavigator";
 
-// Ensure localization gets English and prevent undefined errors
-jest.mock("expo-localization", () => {
-  const original = jest.requireActual("expo-localization");
-  return {
-    __esModule: true,
-    ...original,
-    getLocales: () => {
-      return [{ languageCode: "en" }];
-    },
-  };
-});
-
 describe("<AppNavigator />", () => {
   test("Component renders default screen", () => {
     render(<AppNavigator />);
